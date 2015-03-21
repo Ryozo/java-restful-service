9727f7af977d358b8f48b1189636d5eb865565c8
Ryozo Watanabe <clearstor@gmail.com>
2015-03-22 01:06:29 +0900

    アプリケーション起動時に初期データ投入を実施

--- src/main/java/net/equj65/rest/App.java
+++ src/main/java/net/equj65/rest/App.java
@@ -1,14 +1,23 @@
 package net.equj65.rest;
 
+import net.equj65.rest.entity.Person;
+import net.equj65.rest.repository.PersonRepository;
+
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.servlet.ServletProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class App {
+public class App implements CommandLineRunner {
+	
+	/** DB初期化のためにRepositoryをInjection */
+	@Autowired
+	PersonRepository repository;
 	
 	/**
 	 * Jersey用Servletの設定.
@@ -22,6 +31,17 @@ public class App {
 		servletBean.addInitParameter(
 				ServletProperties.JAXRS_APPLICATION_CLASS, JerseyConfig.class.getName());
 		return servletBean;
+	}
+	
+	/**
+	 * DBに対して初期データを投入
+	 */
+	@Override
+	public void run(String... arg0) throws Exception {
+		repository.save(new Person("person1", 10));
+		repository.save(new Person("person2", 20));
+		repository.save(new Person("person3", 30));
+		repository.save(new Person("person4", 40));
 	}
 	
 	public static void main(String[] args) {
--- src/main/java/net/equj65/rest/entity/Person.java
+++ src/main/java/net/equj65/rest/entity/Person.java
@@ -3,11 +3,17 @@ package net.equj65.rest.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 /**
  * ユーザを表すエンティティクラス.
@@ -15,6 +21,10 @@ import lombok.Data;
  */
 @Entity
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
+@Table(name="Persons")
 public class Person {
 
 	/** Personを一意に識別するID */
@@ -23,13 +33,15 @@ public class Person {
 	private Long personId;
 
 	/** Personの名前 */
+	@NonNull
 	@NotNull
 	@Size(max=255)
 	private String name;
 
 	/** Personの年齢 */
+	@NonNull
 	@NotNull
 	@Min(0)
-	private int age;
+	private Integer age;
 
 }
