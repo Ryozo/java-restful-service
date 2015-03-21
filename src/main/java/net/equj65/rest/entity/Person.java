06bd995276834124d72aa9709485c5bc20133b87
Ryozo Watanabe <clearstor@gmail.com>
2015-03-22 01:13:56 +0900

    リファクタリング

--- src/main/java/net/equj65/rest/entity/Person.java
+++ src/main/java/net/equj65/rest/entity/Person.java
@@ -8,7 +8,6 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -19,11 +18,11 @@ import lombok.ToString;
  * ユーザを表すエンティティクラス.
  * @author ryozo
  */
-@Entity
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
 @ToString
+@Entity
 @Table(name="Persons")
 public class Person {
 
